d964201ea63de93aed1a2ee0d3b1c6060d98b701
Charles Frappa <extern.charles.frappa@europcar.com>
2022-12-21 10:53:02 +0100

    update upload_* script, readme and add download_do.sh script

--- upload_doc.sh
+++ upload_doc.sh
@@ -2,11 +2,21 @@
 
 
 SERVICE_ID=122abd40-9a2a-420f-9417-83e3d1060db8
-FILENAME=PetStore.md
+FILENAME=Petstore.md
 
 DOCUMENT=$(cat $FILENAME | jq -sR .)
 
-echo "Uploading Petstore.md to Service $SERVICE_ID"
+DOCUMENT_ID=$(curl -s -H "Authorization: Bearer ${DECK_KONNECT_TOKEN}" -X GET https://us.api.konghq.com/konnect-api/api/service_packages/${SERVICE_ID}/documents | jq -r '.data | .[0] | .id')
+
+if [ ${DOCUMENT_ID} != null ]
+then
+       echo "Deleting current document id: ${DOCUMENT_ID}"
+       curl -s -H "Authorization: Bearer ${DECK_KONNECT_TOKEN}" -X DELETE https://us.api.konghq.com/konnect-api/api/service_packages/${SERVICE_ID}/documents/${DOCUMENT_ID}
+       echo "Successfully deleted ${DOCUMENT_ID} from service id ${SERVICE_ID}"
+fi
+
+
+echo "Uploading ${FILENAME} to Service $SERVICE_ID"
 
 generate_document_data()
 {
@@ -23,5 +33,5 @@ curl -s -H "Authorization: Bearer ${DECK_KONNECT_TOKEN}" -X POST https://us.api.
     -H 'Content-Type: application/json' \
     -d "$(generate_document_data)" > /dev/null
 
-echo "Succesfully uploaded $FILENAME to Service $SERVICE_ID"
+echo "Successfully uploaded $FILENAME to Service $SERVICE_ID"
 
--- upload_spec.sh
+++ upload_spec.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 SERVICE_VERSION_ID=83792720-e784-4e7a-99ac-7d347920fa40
-FILENAME=PetStore_oas2.yaml
+FILENAME=Petstore_oas2.yaml
 
 SPEC=$(cat $FILENAME | jq -sR .)
+
+if [ ${DOCUMENT_ID} != null ]
+then
+	echo "Deleting current document id: ${DOCUMENT_ID}"
+        curl -s -H "Authorization: Bearer ${DECK_KONNECT_TOKEN}" -X DELETE https://us.api.konghq.com/konnect-api/api/service_versions/${SERVICE_VERSION_ID}/documents/${DOCUMENT_ID}
+	"Successfully deleted ${DOCUMENT_ID} from Service version id ${SERVICE_VERSION_ID}"
+fi
 
 echo "Uploading spec ($FILENAME) to Service Version $SERVICE_VERSION_ID"
 
@@ -12,7 +19,7 @@ generate_document_data()
     cat <<EOF
 {
     "published": "true",
-    "path": "/Petstore_oas2.yaml",
+    "path": "/$FILENAME",
     "content": ${SPEC}
 }
 EOF
